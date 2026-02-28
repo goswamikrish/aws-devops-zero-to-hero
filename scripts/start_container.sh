68347e1832e91f05bf70f8129aa40ebba7264524
goswamikrish <goswamisohum@gmail.com>
2026-02-28 14:01:04 +0530

    Update Docker image name in start_container.sh

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Pull the Docker image from Docker Hub
-docker pull krishsoh/flask-app
+docker pull krishsoh/simple-python-flask-app
 
 # Run the Docker image as a container
-docker run -d -p 5000:5000 krishsoh/flask-app
+docker run -d -p 5000:5000 krishsoh/simple-python-flask-app
