42e058315d80430ca7e358e00f606d83a37732f9
goswamikrish <goswamisohum@gmail.com>
2026-02-28 21:37:27 +0530

    Update container image name and stop command
    Updated image name to include the latest tag and modified container stop command.

--- scripts/stop_container.sh
+++ scripts/stop_container.sh
@@ -2,11 +2,10 @@
 set -e
 
 CONTAINER_NAME="simple-python-flask-app"
-IMAGE_NAME="krishsoh/flask-app"
+IMAGE_NAME="krishsoh/simple-python-flask-app:latest"
 
-echo "Stopping old container if exists..."
-docker stop $CONTAINER_NAME || true
-docker rm $CONTAINER_NAME || true
+echo "Stopping old container..."
+docker rm -f $CONTAINER_NAME || true
 
 echo "Pulling image..."
 docker pull $IMAGE_NAME
